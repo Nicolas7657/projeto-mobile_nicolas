a9bbb2dfd673e30898bab7d1543aa439b47e07eb
Nicolas7657 <165785857+Nicolas7657@users.noreply.github.com>
2024-08-28 08:58:34 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -4,24 +4,34 @@ function adicionarValor() {
 
   const valor = Number(document.getElementById('valor').value);
 
-if (!isNaN(valor)) {
-    vetor.push(valor);
+  if (!isNaN(valor)) {
+      vetor.push(valor);
 
-    const tabela = document.getElementById('tabelaValores').getElementsByTagName('tbody')[0];
+      const tabela = document.getElementById('tabelaValores').getElementsByTagName('tbody')[0];
 
-    const novalinha = tabela.insertRow();
-    const celula = novalinha.insertCell(0);
-    celula.textContent = valor;
+      const novalinha = tabela.insertRow();
+      const celula = novalinha.insertCell(0);
+      celula.textContent = valor;
 
-    document.getElementById('valor').value = '';
-    document.getElementById('valor').focus();
+      document.getElementById('valor').value = '';
+      document.getElementById('valor').focus();
+    }
+}
+
+function mostrarPares() {
+
+    const tabelaPares = document.getElementById('tabelaPares').getElementsByTagName('tbody')[0];
+
+    tabelaPares.innerHTML = ''; // Limpa a tebela antes de adicionar novos Valores
+
+    vetor.forEach(valor => {
+
+            const novoValor = valor + 5;
+
+            const novaLinha = tabelaPares.insertRow();
+            const celula = novaLinha.insertCell(0);
+            celula.textContent = novoValor;
+
+
+    });
   }
-}
-
-/*  function mostrarPares(){
-
-    const tabela = document.getElementById('tabelaValores').getElementByTagName('tbody')[0];
-
-
-}
-*/
